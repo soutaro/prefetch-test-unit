b7732cd213835e4bb80fa25be3615d19e0cc48bb
Soutaro Matsumoto (felix) <matsumoto@soutaro.com>
2011-07-17 00:06:11 +0900

    Initial import

---
+++ ptestunit.rb
@@ -0,0 +1,68 @@
+require "socket"
+
+ENV["RAILS_ENV"] = 'test'
+
+args = ARGV.dup
+
+$interept = false
+
+case ARGV.shift
+when "server"
+  while true
+    STDERR.puts "starting"
+    system "ruby #{__FILE__} test"
+    STDERR.puts "finish"
+  end
+when "test"
+  require "stringio"
+  require "test/unit"
+  require "pathname"
+  
+  require File.expand_path(Dir.pwd + "/config/environment")
+  Rails.application.config.cache_classes = false
+
+  $LOAD_PATH.insert(0, Pathname(Rails.root) + "lib")
+  $LOAD_PATH.insert(0, Pathname(Rails.root) + "test")
+  
+  s = TCPServer.open(8080)
+  socks = [s]
+  
+  _, port, a, b = s.addr
+  puts port
+  
+  puts "ready !"
+  
+  io = s.accept
+  
+  files = []
+  while true
+    line = io.gets.chomp
+    break if line == ""
+    files << line
+  end
+  
+  MiniTest::Unit.output = io
+  
+  files.each do |f|
+    next if f =~ /^-/
+    
+    begin
+      if f =~ /\*/
+        FileList[f].to_a.each {|fn| require File.expand_path(fn) }
+      else
+        require File.expand_path(f)
+      end
+    end
+  end
+  
+when "run"
+  s = TCPSocket.open("localhost", 8080)
+  ARGV.each {|x|
+    s.puts x
+  }
+  s.puts ""
+  
+  while c = s.read(1)
+    print c
+  end
+end
